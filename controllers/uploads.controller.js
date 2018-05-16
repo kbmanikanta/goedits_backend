61197a08bd74a1eb72fd810097c1fbf96ec458f3
kbmanikanta90@gmail.com <kbmanikanta90@gmail.com>
2018-05-16 12:37:51 +0530

    New goedits backend

---
+++ controllers/uploads.controller.js
@@ -0,0 +1,40 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var uploadsService = require('services/uploads.service');
+var multer  = require('multer')
+var multerAzure = require('multer-azure');
+// var upload = multer({ dest: 'uploads/' })
+
+
+var upload = multer({ 
+  storage: multerAzure({
+    connectionString: 'DefaultEndpointsProtocol=https;AccountName=jockey;AccountKey=fgov28LM7Ar9Ge1VqeP6yGAHEvvsY9k3HkY6/HlP2DitTV20gI3ad3tLVfQXy0bpLiQLOMHpjwLYMARsnH6TTQ==;EndpointSuffix=core.windows.net', //Connection String for azure storage account, this one is prefered if you specified, fallback to account and key if not.
+    container: 'uploads',  //Any container name, it will be created if it doesn't exist
+    blobPathResolver: function(req, file, callback){
+      var blobPath = yourMagicLogic(req, file);//Calculate blobPath in your own way.
+      callback(null, blobPath);
+    }
+  })
+})
+
+// routes
+router.post('/',upload.any(),uploadfiles);
+
+function yourMagicLogic(request,filename)
+{
+	return "client1/" + filename.originalname;
+}
+
+function uploadfiles(req, res, next) {
+	    uploadsService.createUpload(req.files,req.body)
+        .then(function () {
+            res.json('success');
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+	
+}
+
+module.exports = router;
---
+++ services/uploads.service.js
@@ -0,0 +1,63 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+var db1 = mongo.db(config.connectionString, { native_parser: true });
+db.bind('orders');
+db1.bind('images');
+var uploadService = {};
+uploadService.createUpload = createUpload;
+
+module.exports = uploadService;
+
+function createUpload(files,body) {
+	 var deferred = Q.defer();
+	console.log(JSON.stringify(body) + " - " + JSON.stringify(files));	
+	var orders = {
+		category :body.category,
+		numofphotos:body.numofphotos,
+		comments:'test comments',
+		orderGUID:'5ab61-3b2d0-9eb03-f3238-e7f97',
+		datecreated:new Date()
+	}
+   var docid = createOrder(orders);
+   
+    var ids = _.each(files, function(p) {
+	 return _.extend(p,{orderGUID:'5ab61-3b2d0-9eb03-f3238-e7f97'});
+	});
+	createImagesOrder(ids);	
+   deferred.resolve();
+    return deferred.promise;
+}
+
+
+
+function createOrder(orders) {
+    var deferred = Q.defer();
+	      db.orders.insert(
+            orders,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+                deferred.resolve(doc);
+            });
+	
+    return deferred.promise;
+}
+
+
+function createImagesOrder(images) {
+    var deferred = Q.defer();
+	      db1.images.insert(
+            images,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+                deferred.resolve(doc);
+            });
+	
+    return deferred.promise;
+}
+
+
